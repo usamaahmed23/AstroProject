a9520e255b9fb8b932373d108a80dd682bdac9c5
usamaahmed2315 <usamaahmed@educative.io>
2025-08-06 21:42:32 +0500

    Initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog,
+};
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection('blog');
+  return rss({
+    title: 'Astro Blog',
+    description: 'A blog built with Astro',
+    site: context.site,
+    items: posts
+      .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+      .map(post => ({
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/blog/${post.slug}/`,
+      })),
+  });
+}
